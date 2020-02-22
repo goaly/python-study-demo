7ae40fe371df18cde483216bf9eef5925a4f3c27
lys <goaly@163.com>
2020-02-22 11:40:27 +0800

    TestHeadlessFirefox

--- openhome/TestHeadlessFirefox.py
+++ openhome/TestHeadlessFirefox.py
@@ -1,4 +1,5 @@
 import xlwt
+import re
 import datetime
 import time
 from selenium import webdriver
@@ -23,6 +24,8 @@ driver.get("https://search.douban.com/book/subject_search?search_text=python&cat
 
 # 隐式等待
 driver.implicitly_wait(10)
+
+now_time_stamp = datetime.datetime.now().strftime('%Y-%m-%d-%H_%M_%S.%f')
 
 
 # 显示等待，WebDriverWait()方法使用
@@ -51,6 +54,7 @@ def maximize_window():
         driver.set_window_size(width + 100, height + 100)
 
 
+# 获取当前页的图书数据
 def get_curpage_data():
     page_data = []
     infos = driver.find_elements_by_xpath('//div[@class="item-root"]/div[@class="detail"]')
@@ -58,7 +62,7 @@ def get_curpage_data():
         str_book_info = info.find_element_by_xpath('div[@class="meta abstract"]').text
         book_infos = str_book_info.split('/')
         if len(book_infos) < 5:
-            continue;
+            continue
 
         title = info.find_element_by_xpath('div[@class="title"]/a')
         url = title.get_attribute('href')
@@ -66,6 +70,9 @@ def get_curpage_data():
 
         author = book_infos[0]
         price = book_infos[-1]
+        if re.search('CNY', price, re.I) or price.find('元'):
+            # 人民币价格，去除非数值信息
+            price = re.sub('[^\d\.]+', '', price)
         pub_date = book_infos[-2]
         publisher = book_infos[-3]
         rate = info.find_element_by_xpath('div[2]/span[2]').text
@@ -77,46 +84,95 @@ def get_curpage_data():
     return page_data
 
 
+# 获取字符串显示在excel单元格中的等效0字符宽度，xlwt中列宽的值表示方法：默认字体0的1/256为衡量单位。
+def get_col_width(txt):
+    # 默认11个字符
+    col_w = 11
+    if txt:
+        col_w = len(txt)
+        zh_char_num = 0
+        for ch in txt:
+            if '\u4e00' <= ch <= '\u9fff':
+                zh_char_num += 1
+        col_w = (col_w - zh_char_num) + zh_char_num * 2
+    return 256 * col_w
+
+
 try:
     # 爬取页数
-    pageCount = 2
+    pageCount = 7
     headers = ['书名', '链接', '作者', '出版社', '出版日期', '价格', '得分', '评价人数']
+    col_default_w = 256 * 11
+    col_widths = [col_default_w, col_default_w, col_default_w, col_default_w, col_default_w, col_default_w,
+                  col_default_w, col_default_w]
     workbook = xlwt.Workbook(encoding='utf-8')
     sheet = workbook.add_sheet('sheet1')
     rowIdx = 1
 
+    # 设置第一行高度
+    tall_style = xlwt.easyxf('font:height 480')  # 24pt
+    first_row = sheet.row(0)
+    first_row.set_style(tall_style)
+
+    # 创建一个样式对象，初始化样式
+    style = xlwt.XFStyle()
+    al = xlwt.Alignment()
+    al.horz = 0x02  # 设置水平居中
+    al.vert = 0x01  # 设置垂直居中
+    style.alignment = al
+
     # 写入表头
     for h, header in enumerate(headers):
-        sheet.write(0, h, header)
+        sheet.write(0, h, header, style)
 
     sheetdata = []
 
-    # 得到执行完js的代码
+    # 得到执行完js的代码True
     with open('pageSource.html', 'w', encoding='utf8') as fp:
         fp.write(driver.page_source)
 
     maximize_window()
     # 截屏
     # driver.save_screenshot(
-    #     'D:/360极速浏览器下载/screenshot_' + datetime.datetime.now().strftime('%Y-%m-%d-%H_%M_%S.%f') + '.png')
+    #     'D:/360极速浏览器下载/screenshot_' + now_time_stamp + '.png')
 
     # 打印网页源代码
     # print(driver.page_source, '\n')
 
-    for n in range(pageCount + 1):
+    # 设置链接字体颜色
+    style_link_color = xlwt.easyxf('font:colour_index blue')
+
+    n = 1
+    while True:
         sheetdata = get_curpage_data()
         if len(sheetdata) > 0:
             for rowData in sheetdata:
                 for j, cellData in enumerate(rowData):
-                    sheet.write(rowIdx, j, cellData)
+                    new_col_w = get_col_width(cellData)
+                    if new_col_w > col_widths[j]:
+                        # 更新列宽
+                        col_widths[j] = new_col_w
+                    if cellData.startswith("http://") or cellData.startswith("https://"):
+                        # 超链接
+                        sheet.write(rowIdx, j, xlwt.Formula('HYPERLINK("' + cellData + '";"' + cellData + '")'), style_link_color)
+                    else:
+                        sheet.write(rowIdx, j, cellData)
                 rowIdx += 1
+        n += 1
+        if n <= pageCount:
             next_link = driver.find_element_by_css_selector('div.paginator > a.next')
-            print('准备跳转第', n + 2, '页：', next_link.get_attribute('href'))
+            print('\n==============================准备跳转第', n, '页：', next_link.get_attribute('href'))
             next_link.click()
             driver.implicitly_wait(3)
-            time.sleep(4)
+            time.sleep(5)
+        else:
+            print('\n==============================爬取完毕==============================')
+            break
 
-    workbook.save('D:/360极速浏览器下载/pythonBookTop150.xls')
+    # 设置列宽
+    for i, col_w in enumerate(col_widths):
+        sheet.col(i).width = col_w
+    workbook.save('D:/360极速浏览器下载/pythonBookTop100_' + now_time_stamp + '.xls')
 
 except NoSuchElementException as ex:
     print('未找到对应元素：', ex.msg)
--- openhome/TestRegExp.py
+++ openhome/TestRegExp.py
@@ -18,6 +18,19 @@ phone = '132-0711-3160'
 newPhone = re.sub('\D+', '', phone)
 print(newPhone)
 
+# sub()用于替换字符串的匹配项
+price1 = '119.00元'
+if price1.find('元'):
+    print(price1)
+new_price1 = re.sub('[^\d\.]+', '', price1)
+print(new_price1)
+
+price2 = 'CNY 69.00'
+if re.search('CNY', price2, re.I):
+    print(price2)
+new_price2 = re.sub('[^\d\.]+', '', price2)
+print(new_price2)
+
 result = re.search('All contents are copyrighted',
                  '2018-2019&nbsp;&copy;&nbsp;所有内容版权归原作者所有 / All contents are copyrighted by their respective authors. <br/> Powered by <a target="_blank" href="/">斗破小说网</a>', re.I)
 print(result is None)
