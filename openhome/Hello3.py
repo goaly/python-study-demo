e27a1bd6362f74cf48ee222ad2181e118b4b87f1
lys <goaly@163.com>
2022-08-07 17:33:10 +0800

    命令行参数从sys.arg

--- openhome/Hello3.py
+++ openhome/Hello3.py
@@ -1,4 +1,5 @@
 from sys import argv
 
 # 需要在运行时加入一个参数，如：python hello3.py tom
+# 通过sys模块argv列表获取命令行参数，sys.arg[0]保存的是源码文件名，命令行参数从sys.arg[1]开始引用
 print('哈啰！', argv[1], '!')
--- openhome/TestFunction.py
+++ openhome/TestFunction.py
@@ -1,4 +1,5 @@
 # code:UTF-8
+# 测试函数定义
 def change_number(number):
     hiding_num = number.replace(number[3:7], '*' * 4)
     print(hiding_num)
