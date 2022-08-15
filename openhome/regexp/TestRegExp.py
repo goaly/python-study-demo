7ced259d69c75dba90b3c788882d629e87285997
liuyongshun <goaly@163.com>
2022-08-15 22:15:49 +0800

    包结构调整

--- openhome/regexp/TestRegExp.py
+++ openhome/regexp/TestRegExp.py
@@ -13,12 +13,12 @@ d = '2022'
 # infos = re.findall('xx(.*?)xx', a)
 # print(infos)
 
-# b = 'one1two2three3'
-# re1 = re.search('\d+', b)
-# print(re1)
-# print(re1.group())
-# re2 = re.findall('\d+', b)
-# print(re2)
+b = 'one1two2three3'
+re1 = re.search('\d+', b)
+print(re1)
+print(re1.group())
+re2 = re.findall('\d+', b)
+print(re2)
 
 # sub()用于替换字符串的匹配项
 # phone = '132-0711-3160'
@@ -40,6 +40,6 @@ d = '2022'
 # new_price2 = re.sub('[^\d\.]+', '', price2)
 # print(new_price2)
 
-result = re.search('All contents are copyrighted',
-                 '2018-2019&nbsp;&copy;&nbsp;所有内容版权归原作者所有 / All contents are copyrighted by their respective authors. <br/> Powered by <a target="_blank" href="/">斗破小说网</a>', re.I)
-print(result is None)
+# result = re.search('All contents are copyrighted',
+#                  '2018-2019&nbsp;&copy;&nbsp;所有内容版权归原作者所有 / All contents are copyrighted by their respective authors. <br/> Powered by <a target="_blank" href="/">斗破小说网</a>', re.I)
+# print(result is None)
