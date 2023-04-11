dad514d85eb09ed999509a495b58a9f28eb6cd76
lys <goaly@163.com>
2023-04-11 18:12:29 +0800

    chrome无界面形态测试

--- openhome/TestHeadlessChrome.py
+++ openhome/TestHeadlessChrome.py
@@ -1,28 +1,31 @@
 import datetime
 from selenium import webdriver
 # 导入 WebDriverWait 包
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import DesiredCapabilities
 
-# from selenium.webdriver.chrome.options import Options
-#
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--no-sandbox')
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+# 不弹出浏览器 - 加了headless不可用，豆瓣网做了反爬
 # chrome_options.add_argument('--headless')
 # chrome_options.add_argument('--disable-gpu')
-# chrome_options.add_argument('--ignore-certificate-errors')
-# capabilities = DesiredCapabilities.CHROME
-# capabilities["ACCEPT_SSL_CERTS"] = True
-# capabilities["acceptSslCerts"] = True
-# capabilities["acceptInsecureCerts"] = True
-# capabilities["ignore-certificate-errors"] = True
-# driver = webdriver.Chrome(options=chrome_options, desired_capabilities=capabilities)
+# chrome_options.add_argument('window-size=1920,1080')
+chrome_options.add_argument('--ignore-certificate-errors')
+capabilities = DesiredCapabilities.CHROME
+capabilities["ACCEPT_SSL_CERTS"] = True
+capabilities["acceptSslCerts"] = True
+capabilities["acceptInsecureCerts"] = True
+capabilities["ignore-certificate-errors"] = True
+driver = webdriver.Chrome(options=chrome_options, desired_capabilities=capabilities)
 
-driver = webdriver.PhantomJS()
+# driver = webdriver.Chrome()
 
 driver.get("https://search.douban.com/book/subject_search?search_text=python&cat=1001&start=0")
-# driver.get("https://www.douban.com")
+
 # 隐式等待
 driver.implicitly_wait(3)
 # 显示等待，WebDriverWait()方法使用
@@ -42,7 +45,7 @@ try:
         fp.write(driver.page_source)
 
         # 最大化显示宽度
-        if driver.find_element_by_tag_name('body'):
+        if driver.find_element(By.TAG_NAME, 'body'):
             width = driver.execute_script(
                 "return Math.max(document.body.scrollWidth, document.body.offsetWidth, "
                 "document.documentElement.clientWidth, document.documentElement.scrollWidth, "
@@ -58,9 +61,9 @@ try:
 
     print(driver.page_source, '\n')
 
-    infos = driver.find_elements_by_xpath('//div[@class="item-root"]/div[@class="detail"]')
+    infos = driver.find_elements(By.XPATH, '//div[@class="item-root"]/div[@class="detail"]')
     for info in infos:
-        title = info.find_element_by_xpath('div[@class="title"]/a')
+        title = info.find_element(By.XPATH, 'div[@class="title"]/a')
         name = title.text
         if name:
             print(name)
