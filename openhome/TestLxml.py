6dd3a34885b8ebfdc5ef70134104bb0069551c7f
lys <goaly@163.com>
2023-04-12 13:36:50 +0800

    lxml测试

--- openhome/TestLxml.py
+++ openhome/TestLxml.py
@@ -2,7 +2,7 @@ import requests
 from lxml import etree
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
 }
 
 res = requests.get('https://book.douban.com/top250', headers=headers)
