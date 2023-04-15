96ad090194d3f03f98c2695ab67c3941d2e60391
lys <goaly@163.com>
2023-04-15 14:29:36 +0800

    测试多进程

--- openhome/TestFunction.py
+++ openhome/TestFunction.py
@@ -2,7 +2,7 @@
 # 测试函数定义
 def change_number(number):
     hiding_num = number.replace(number[3:7], '*' * 4)
-    print(hiding_num)
+    return hiding_num
 
 
-change_number('13207113160')
+print(change_number('13207113160'))
